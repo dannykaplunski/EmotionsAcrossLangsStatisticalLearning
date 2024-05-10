1dc6cfa971368ca8895d6f84d2503582adc277b0
sarapage88 <165282245+sarapage88@users.noreply.github.com>
2024-05-10 06:03:38 +0300

    Update script.py
    Added comments

--- script.py
+++ script.py
@@ -52,6 +52,7 @@ def estimate_syllabic_rate(audio_data, sr, duration_in_seconds):
 def get_spectral_envelope(y, sr ):
     spectral_centroids = librosa.feature.spectral_centroid(y=y, sr=sr)[0] 
     return np.mean(spectral_centroids), np.median(spectral_centroids), np.min(spectral_centroids), np.max(spectral_centroids), np.std(spectral_centroids), np.percentile(spectral_centroids, 75) - np.percentile(spectral_centroids, 25), np.mean(np.diff(spectral_centroids)), np.std(np.diff(spectral_centroids)), scipy.stats.skew(spectral_centroids), scipy.stats.kurtosis(spectral_centroids)
+    #**** desc of returns here****
 
 #Mel-frequency cepstral coefficients MFCCs - effectively represent the short-term power spectrum of sound, and are used in automatic speech and speaker recognition.
 # Mel Filterbank: The audio signal is first converted to the Mel scale, which approximates the human auditory system's response more closely than the linearly-spaced frequency bands used in the Fourier transform.
@@ -63,8 +64,9 @@ def extract_mfcc_features(y, sr, n_mfcc=13):
     mfccs_mean = mfccs.mean(axis=1)
     mfccs_std   = mfccs.std(axis=1)
     return mfccs_mean, mfccs_std
+    #**** desc of returns here****dimensions???
 
-#spliting the audio data into n parts
+#splitting the audio data into n parts ****what is y?****
 def split_audio(y, num_parts):
     total_samples = len(y)
     part_length = total_samples // num_parts
@@ -72,53 +74,86 @@ def split_audio(y, num_parts):
     if total_samples % num_parts != 0:
         parts[-1] = np.concatenate((parts[-1], y[num_parts * part_length:]))
     return parts
+    #**** desc of returns here****
 
-
+#Gets basic stats of the audio wave
 def get_mMa(clip):
     max_amp = np.max(clip)
     min_amp = np.min(clip)
     mean_amp = np.mean(clip) 
     med_amp = np.median(clip)
-    return max_amp, min_amp, mean_amp, med_amp
+    return max_amp, min_amp, mean_amp, med_amp 
+    #max signal amplitude, min signal amplitude, mean signal amplitude, median signal amplitude
 
+#Gets the fourier transform information
 def get_fourier(clip, samp_rate):
+    #Get the Fourier transform
     fourier = np.fft.fft(clip)
+    #Get the spectrum
     magspectrum = np.abs(fourier)
+    #get the frequencies that correspond to the fourier magnitudes
     frequencies = abs(np.fft.fftfreq(len(fourier), d=1/samp_rate)) 
+    #get the f with max magnitude
     magmax = np.max(magspectrum) 
+    #find the index
     fmaxindex = np.where(magspectrum == magmax)
+    #get the corresponding frequency of maximum power at any point during the clip. If there are 2 points, pick one.
     maxpf = frequencies[int(np.max(fmaxindex[0]))] 
+    #define max f as highest f with 1/3 of the peak frequency
     isrelmax = torch.tensor(magspectrum > magmax/3).to(torch.float)
-    relfs = torch.tensor(frequencies)*isrelmax 
+    relfs = torch.tensor(frequencies)*isrelmax #frequencies over 1/3 max mult by 1, others zeroed out
+    #maximum relevant frequency
     maxf = max(relfs).numpy() 
+    #define min f as lowest f with 1/3 of the peak (may often be 0)
     minf = min(relfs).numpy()
+    #the BW of the magnitudes is max-min
     bwfourier = maxf-minf
     return magmax, maxpf, maxf.item(), minf.item(), bwfourier
+    #max magnitude of fourier, f for max magnitude, highest relevant f, lowest relevant f, range (max-min) relevant f
 
+#get basic spectral stats
 def get_spec(clip, hoplen, samp_rate, nfft, maxf):
+    #Get BW
     bw = np.max(librosa.feature.spectral_bandwidth(y=clip, sr=samp_rate, hop_length=hoplen)[0]) #sometimes gives 2 items in vector, one can be 0
+    #Get the spectral rolloff
     specroll = np.max(librosa.feature.spectral_rolloff(y= clip, sr=samp_rate, n_fft=nfft, hop_length=hoplen, roll_percent=0.85)[0])#sometimes gives 2 items in vector, one can be 0
+    #get fundamental frequency
     try:
         f0 = np.nanmean(librosa.pyin(y=clip, fmin = 60, fmax = maxf, sr=samp_rate)[0])
     except RuntimeWarning:
+    #sometimes it doesn't give a fundamental frequency and thinks it's nothing
         f0 = 0
     return bw, specroll, f0
+    #spect energy BW, spectral rolloff 85% energy, fundamental freq (f0)
 
+#get the frequencies with the max power (spectrogram) and spectral centroid
 def get_fpwr(clip, nfft, hoplen, f_res, samp_rate):
     fourier = librosa.stft(clip, n_fft=nfft, hop_length=hoplen)
+    #convert it from raw values to dB (log)
     findb = librosa.amplitude_to_db(abs(fourier))
+    #0 is the frequency bins, 1 is the time stamp
+    #frequency corresponding to each row
     f_bins = [i * f_res for i in range(fourier.shape[0])]
+    #get the max energy value
     overallmaxdb = np.max(findb) 
+    #find the row it's in
     overallindex = np.where(findb == overallmaxdb)
-    maxptf=f_bins[int(overallindex[0])] 
+    #get the corresponding frequency for that row
+    maxptf=f_bins[int(overallindex[0])] #frequency of maximum power at any point during the clip
+    #sum up the energy for all time periods for each row
     rowsums = np.sum(fourier, axis=1)
+    #get the max energy value
     rowmax = np.max(rowsums)
+    #find the row it's in
     sumindex = np.where(rowsums == rowmax)
-    maxsumf=f_bins[int(sumindex[0])] 
+    #get the corresponding frequency for that row
+    maxsumf=f_bins[int(sumindex[0])] #frequency of maximum power throughout the clip
     centroidvect = librosa.feature.spectral_centroid(y=clip, sr=samp_rate)
     meancent = np.mean(centroidvect)
     return maxptf, maxsumf, meancent
+    #freq with max power at any point, freq with max overall power, spectral centroid
 
+#Gets differences and gradients between clip segment data ****do we still need this since you did it differently with the dataframe?****
 def diffgrad(intensor):
     cols = intensor.shape[1]
     rows = intensor.shape[0]
@@ -126,14 +161,16 @@ def diffgrad(intensor):
     lastt = torch.cat((intensor, zerost), dim=0)
     nextt = torch.cat((zerost, intensor), dim=0)
     diffs = nextt-lastt
-    grads = nextt/lastt 
+    grads = nextt/lastt #****need to replace nans with 0s ****
+    #remove the last line since it doesn't have meaning
     tensoroutput = torch.cat((diffs[0:rows], grads[0:rows]), dim=1)
     return tensoroutput
+    #first half of tensor is the raw difference between segments. second half of the tensor is the % difference between segments
 
 def get_features(file_path):
     n = 5
     df = pd.DataFrame()
-    try: #getting "clean" audio data with triming, sample rate, and bit depth
+    try: #getting "clean" audio data with trimming, sample rate, and bit depth
         audio_data, sr = librosa.load(file_path)
         audio_data, index =  librosa.effects.trim(audio_data, top_db=30)
         sample_rate, new = wavfile.read(file_path)
@@ -193,7 +230,7 @@ def get_features(file_path):
     df[['zcr_frequncys_from_average' + str(x)  for x in range(len(splited_data) - 1)]] =  df[['zcr_frequncys_' + str(x)  for x in range(len(splited_data) - 1)]] - list(df['frequncys_zcr']) * (len(splited_data) - 1)
     df[['zcr_frequncys_diffs_from_average' + str(x)  + '_' + str(x+1) for x in range(len(splited_data) - 1)]] =  df[['zcr_frequncys_diffs_' + str(x)  + '_' + str(x+1) for x in range(len(splited_data) - 1)]]- list(df['frequncys_zcr_diff']) * (len(splited_data) - 1) 
 
-    #get syllabic_rates  for each part and there diffs
+    #get syllabic_rates  for each part and their diffs
     syllabic_rates = [estimate_syllabic_rate(x, sr, duration / (n)) for x in splited_data]
     df[['syllabic_rate_' + str(x) for x in range(len(splited_data))]] = syllabic_rates
     df[['syllabic_rate_diffs_' + str(x)  + '_' + str(x+1) for x in range(len(splited_data) - 1)]] = np.diff(syllabic_rates)
@@ -220,22 +257,23 @@ def get_features(file_path):
     #get spectral stats
     bw, specroll, f0 = get_spec(audio_data, hoplwhole, sr, nfft, maxf)
     df[['bw', 'specroll', 'f0']] = [bw, specroll, f0]
-
     #0) spect energy BW, 1) spectral rolloff 85% energy, 2) fundamental freq (f0)
+    
     #get normalized range/f0
     rangef0 = minf_items/maxpf
     df['rangef0'] = [rangef0]
     #get spectrogram stats
     maxptf, maxsumf, meancent = get_fpwr(audio_data, nfft, hoplwhole, fres, sr)
     df[['maxptf', 'maxsumf', 'meancent']] = [maxptf, maxsumf, meancent]
-
     # 0) freq with max power at any point, 1) freq with max overall power, 2) spectral centroid
     #and normalize them
     df[['maxptf/meancent', 'maxsumf/meancent']] =[maxptf/meancent, maxsumf/meancent]
+    
     #get chroma data
     chroma = librosa.feature.chroma_stft(y=audio_data, sr=sr, hop_length=hoplwhole, n_fft=nfft)
     #can give 1 or 2 columns. need to either max or avg them, not sure which makes sense yet
-    #get data for the different segments of the file************************
+    
+    #get data for the different segments of the file-----------------------
     #Makes a setup vector for segmenting the audio file into subclips
     #number of sections to split into +1
     #the shortest filess are 1 second and we don't want clips shorter than 200 ms since that's what the human ear picks up
@@ -276,8 +314,9 @@ def get_features(file_path):
         for i in range(0, n - 1):  # up to 4 because we compute difference with the next section
             df[f'{col}_{i}_diff'] = df[f'{col}_{i}'] - df[f'{col}_{i+1}']
             df[f'{col}_{i}_grad'] = df[f'{col}_{i}'] / df[f'{col}_{i+1}']
+    #****need to tell it what to do when dividing by 0****
 
-
+#****comments????****
 def df_func():
     not_ok_array = []
     df = pd.DataFrame()
@@ -297,6 +336,7 @@ def df_func():
     df = create_general_statistics(df)
     return df, not_ok_array
 
+#****comments????****
 def specific_df_func(name):
     new_df = pd.DataFrame()
     new_df = get_features("wav-files/" + name)
